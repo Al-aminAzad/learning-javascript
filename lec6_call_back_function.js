f4e005051893bd8bb0035fca8163ce1f673fa864
Al-aminAzad <alaminazad240@gmail.com>
2020-12-15 17:09:44 +0600

    lecture6 is added.it is all about callback function

---
+++ lec6_call_back_function.js
@@ -0,0 +1,56 @@
+function hello(name) {
+  print();
+}
+function print(arg) {
+  console.log("Hello" + " " + arg);
+}
+print("Alamin");
+
+function hello(name, print) {
+  print(name);
+}
+
+hello("Alamin", function (a) {
+  console.log("Hello" + " " + a);
+});
+hello("Alamin", function (a) {
+  console.log("Hi how are you" + " " + a + "?");
+});
+hello("Azad", function (a) {
+  console.log("Length of name is " + a.length);
+});
+// function print(a) {
+//   console.log();
+// }
+
+var me = {
+  name: "Al-amin",
+  age: 18,
+  email: "alaminazad240@gmail.com",
+};
+
+function printmyself(person, callback) {
+  console.log("Person: " + person.name + "(" + person.age + ")");
+  if (person.age >= 18) {
+    callback(person.email);
+  }
+}
+
+printmyself(me, function (email) {
+  console.log("Email sent to " + email);
+});
+
+function print(data, callback1, callback2) {
+  console.log("Main data is: " + data);
+  callback1(data);
+  callback2(data);
+}
+print(
+  "Hello I am alamin. I am learning Javascript",
+  function (data) {
+    console.log("Uppercase: " + data.toUpperCase());
+  },
+  function (data) {
+    console.log("Lowercase: " + data.toLowerCase());
+  }
+);
