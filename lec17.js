7b54480544d131fc8e6aaae10da1b4b80b9da6ec
Al-aminAzad <alaminazad240@gmail.com>
2020-12-21 17:58:59 +0600

    Hoisting(behind the scene of javascript)

---
+++ lec17.js
@@ -0,0 +1,22 @@
+// console.log(x);
+// var x=10;
+// print('Jayeema');
+// function print(name)
+// {
+//     console.log(name);
+// }
+// print('Alamin');
+// deeclaretion();
+// function deeclaretion()
+// {
+//     console.log('I am callable from anywhere');
+// }
+// deeclaretion();
+//expression();
+console.log('first ecpression '+expression);
+var expression = function()
+{
+    console.log('I am a function expression');
+}
+console.log('second ecpression '+expression);
+expression();
