1c6eb96ee3ac1a0589ce13982190ecdd48eed9d0
Al-aminAzad <alaminazad240@gmail.com>
2020-12-21 18:39:57 +0600

    Scope(Lexical scoping)

---
+++ lec18.js
@@ -0,0 +1,46 @@
+// var a=100;
+//console.log(a);
+
+// function first()
+// {
+//     console.log(a);
+//     function second()
+//     {
+//         var b=200;
+//         console.log(a);
+//         //console.log(d);
+//         function third()
+//         {
+//             var d=300;
+//             console.log(b);
+//         }
+//         third();
+//     }
+//     second();
+    //third();
+// }
+// first();
+
+// function localscope()
+// {
+//     var local=' I am local variable';
+//     console.log(local);
+// }
+// localscope();
+// console.log(local);
+
+// var person={
+//     name:'alamin',
+//     age:25
+// }
+// console.log(person);
+function objectFunc()
+{
+    var person={
+        name:'alamin',
+        age:25
+    };
+     console.log(person);
+}
+//console.log(person);
+objectFunc();
