ab34c2f2c4c45e444bc57cf0110aab694fc7491c
Al-aminAzad <alaminazad240@gmail.com>
2020-12-23 23:53:05 +0600

    Template literal / template string and multiline string

---
+++ ES6_2.js
@@ -0,0 +1,31 @@
+// const name='Alamin'
+// const age=25;
+// const email = 'alaminazad240@gmail.com';
+// // const details = 'name: ' + name + ' age: ' +age+' email: '+email;
+// const details = `name:${name} age:${age} email:${email}`;
+
+// console.log(details);
+var name = 'Alamina azad',
+    age=25,
+    work='student';
+    console.log('My name is '+name+' and I\'m '+age+' years old!. Curently I\'m a '+work);
+ const name6='jayeema',
+       age6=24,
+       work6='Jobholder';
+       console.log(`My name is ${name6} and I\'m ${age6}
+ years old. Curently I\'m a ${work6}`);
+
+ function calculateage6(age)
+ {
+     return `I'm ${2020-age} years old`;
+ }
+ calculateage6(1995);
+const start='My name is Alamin';
+start.startsWith('My');
+const ends='I love Programming';
+ends.endsWith('My');
+const incld='I love programming with javascript';
+incld.includes('with');
+const rpt='Hello world';
+rpt.repeat(5);
+console.log(`I want say ${'Alhamdulillah '.repeat(5)}`);
