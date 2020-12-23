0fcbc513113d7aa2e54118be7130ed5210d72595
Al-aminAzad <alaminazad240@gmail.com>
2020-12-23 18:52:56 +0600

    ES6(javascript)-let,const

---
+++ ES6_1.js
@@ -0,0 +1,80 @@
+// const x=20;
+// x=25;
+// console.log(x);
+// const arr=[];
+// arr.push(10);
+// arr.push(20);
+// console.log(arr);
+
+// let a=10;
+// if(true)
+// {
+//     let a=25;
+//     console.log('Indise a= ',a);
+// }
+// console.log('Outsie a= ',a);
+
+// function demo()
+// {
+//     {
+//         let x=10;
+//         {
+//             const x=25;
+//             console.log(x);
+//         }
+//          x=50;
+//         // console.log(x);
+//     }
+//     //x=60;
+//     console.log(x);
+// }
+// demo()
+
+// var a='Alamin azad';
+// console.log(a);
+// a='Jayeema';
+// console.log(a);
+
+// let a='Alamin azad';
+// console.log(a);
+// a='Jayeema';
+// console.log(a);
+// const b='Alamin azad';
+// console.log(b);
+// b='Jayeema';
+// console.log(b);
+// function es5var()
+// {
+//     if(true)
+//     {
+//         var a=10;
+//     }
+//     console.log('Value a= ',a);
+// }
+// es5var();
+
+// function es6var()
+// {
+//     if(true)
+//     {
+//         const a=25;
+//     }
+//     console.log('value a= ',a);
+// }
+// es6var();
+// console.log(es5var);
+// var es5var=10;
+// console.log(es6var);
+// const es6var=10;
+var i=12;
+for (var i=0;i<5;i++)
+{
+    console.log(i);
+}
+console.log('value:' +i);
+let i6=12;
+for(let i6=0;i6<5;i6++)
+{
+    console.log(i6);
+}
+console.log('value: ',i6);
