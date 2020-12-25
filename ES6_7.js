aab7a33f44e08dfa30a390458f9c53347661740f
Al-aminAzad <alaminazad240@gmail.com>
2020-12-26 02:30:32 +0600

    module(export-import, export-require

---
+++ ES6_7.js
@@ -0,0 +1,11 @@
+// import * as math from './math.js'
+// console.log(add(10,12));
+// console.log(math.add(15,14));
+
+// import {add,div} from './math.js';
+// console.log(add(25,6));
+// console.log(div(10,5));
+const mod= require('./math.js');   
+//x.namefun('Alamin',22);
+mod.namefun('Alamin',22);
+console.log(mod.p);
---
+++ math.js
@@ -0,0 +1,11 @@
+// export const add= (a,b)=>a+b;
+// export const sub = (a,b)=>a-b;
+// export const times= (a,b)=>a*b;
+// export const mod = (a,b)=>a%b;
+// export const div = (a,b)=>a/b;
+
+const namefun = (name,age)=>console.log(`My name is ${name} and ${age} years old`);
+const sum = (a,b)=>a+b;
+const p=3.1416;
+
+module.exports={namefun,sum,p};
